683003f870061dc9a46fcba1b204ea1aca9c37d6
zrma <bulbitain@nhnnext.org>
2014-07-18 13:04:12 +0900

    이진 탐색 트리 기본 골격 업로드
    ㅇㅅㅇ

---
+++ HW.C1.1/HW.C1.1/BinarySearchTree.h
@@ -0,0 +1,29 @@
+﻿#pragma once
+
+
+#define NOT_A_NUMBER	INT_MAX
+
+struct Node
+{
+	int		data = NOT_A_NUMBER;
+	Node	*left = nullptr;
+	Node	*right = nullptr;
+};
+
+class BinarySearchTree
+{
+public:
+	BinarySearchTree() { }
+	~BinarySearchTree() { DestroyTree(); }
+
+	void	Insert( int key );
+	Node*	Search( int key );
+	void	DestroyTree();
+
+private:
+	void	Insert( int key, Node *leaf );
+	Node*	Search( int key, Node *leaf );
+	void	DestroyTree( Node *leaf );
+
+	Node*	m_Root = nullptr;
+};
